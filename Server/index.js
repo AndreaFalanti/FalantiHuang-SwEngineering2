42b0b8c1a734d30a05d00704f76ef03fa3536907
AndreaFalanti <a.falanti@gmail.com>
2019-12-16 22:42:08 +0100

    Add and setup useful packages

--- Server/index.js
+++ Server/index.js
@@ -7,18 +7,28 @@ var fs = require('fs'),
 var app = require('connect')();
 var oas3Tools = require('oas3-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 8080;
+var serverPort = process.env.PORT || 8080;
+
+let dirToOther = path.join(__dirname, 'other');     // points "Other" folder
+
+let cookieSession = require("cookie-session");
+let cookieParser = require("cookie-parser");
+let serveStatic = require("serve-static");
 
 // swaggerRouter configuration
 var options = {
-  swaggerUi: path.join(__dirname, '/swagger.json'),
-  controllers: path.join(__dirname, './controllers'),
+  swaggerUi: path.join(dirToOther, '/swagger.json'),
+  controllers: path.join(dirToOther, './controllers'),
   useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
 };
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
+var spec = fs.readFileSync(path.join(dirToOther,'api/swagger.yaml'), 'utf8');
 var swaggerDoc = jsyaml.safeLoad(spec);
+
+// Add cookies to responses
+app.use(cookieParser());
+app.use(cookieSession({name: "session", keys: ["id", "accountType"]}));
 
 // Initialize the Swagger middleware
 oas3Tools.initializeMiddleware(swaggerDoc, function (middleware) {
@@ -34,6 +44,8 @@ oas3Tools.initializeMiddleware(swaggerDoc, function (middleware) {
 
   // Serve the Swagger documents and Swagger UI
   app.use(middleware.swaggerUi());
+
+  app.use(serveStatic(path.join(__dirname, "public")));
 
   // Start the server
   http.createServer(app).listen(serverPort, function () {
